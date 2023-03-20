fd07fb4e56c5e76edb84c42dbc1f479ca9b0f029
EkaterinaSterhova <126870774+EkaterinaSterhova@users.noreply.github.com>
2023-03-20 14:27:27 +0600

    число А в степни В

---
+++ Задача 25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Нельзя использовать функцию Math.Pow()!
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear(); 
+Console.WriteLine("Введите два числа: "); 
+int A = int.Parse(Console.ReadLine()!); 
+int B = int.Parse(Console.ReadLine()!); 
+int result = A; 
+ 
+ 
+for (int i = 1; i <= B; i++) 
+{ 
+    result = result * A; 
+} 
+ Console.WriteLine(result);
+
